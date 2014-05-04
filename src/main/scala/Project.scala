ee6c37c7c3e053e973ffb3082c1bc53e18041c83
todesking <discommunicative@gmail.com>
2014-05-04 22:33:02 +0900

    shutdown

--- src/main/scala/Project.scala
+++ src/main/scala/Project.scala
@@ -19,7 +19,13 @@ object ProjectSetting {
     def keywordMap(sexp:SExp):Either[String, KeyMap] =  sexp match { case l:SExpList => Right(l.toKeywordMap) case s => Left(s"Not List: ${s.toReadableString}") }
     for {
       ensime <- keywordMap(SExp.read(new CharSequenceReader(source.getLines().filter(!_.startsWith(";;")).mkString("\n")))).right
-      project <- (ensime.get(key(":subprojects")) match { case Some(l:SExpList) => l.headOption match {case Some(a) => keywordMap(a) case None => Left(":subprojects is empty")} case _ => Left(":subprojects error") }).right
+      project <- (ensime.get(key(":subprojects")) match {
+        case Some(l:SExpList) => l.headOption match {
+          case Some(a) => keywordMap(a)
+          case None => Left(":subprojects is empty")
+        }
+        case _ => Left(":subprojects error")
+      }).right
       compileDeps <- (project.get(key(":compile-deps")) match {
         case Some(l:SExpList) => Right(l.toSeq.flatMap { _ match { case s:StringAtom => Seq(s.value) case _ => Seq() } })
         case _ => Left("invalid :compile-deps")
@@ -35,6 +41,8 @@ class Project(root: String, setting:ProjectSetting) {
   val nscSettings:Settings = new Settings()
   nscSettings.processArguments(List("-classpath", setting.classPath.mkString(":")), true)
   val global:Global = new Global(nscSettings, new ConsoleReporter(nscSettings))
+
+  def shutdown():Unit = global.askShutdown()
 }
 
 object Project {
@@ -53,5 +61,6 @@ object Main {
     val project = Project.load(".") match { case Right(p) => p; case Left(msg) => throw new RuntimeException(s"load failed: $msg") }
     val sourceFile:SourceFile = project.global.getSourceFile("./src/main/scala/Project.scala")
     val response : Response[project.global.Tree] = new Response()
+    project.shutdown()
   }
 }
